6c6b796c66754227606dfe2b6f541d28a23f0716
Marianna Hanina <Marianna_Hanina@epam.com>
2015-12-05 17:49:22 +0300

    add bower, sass and web server

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,71 @@
+"use strict";
+
 var gulp = require('gulp'),
-	concatCss = require('gulp-concat-css'),
+	//concatCss = require('gulp-concat-css'),
 	rename = require('gulp-rename'),
 	minifyCss = require('gulp-minify-css'),
-	notify = require('gulp-notify');
+	notify = require('gulp-notify'),
+	autoprefixer = require('gulp-autoprefixer'),
+	connect = require('gulp-connect'),
+	livereload = require('gulp-livereload'),
+	sass = require('gulp-sass'),
+	uncss = require('gulp-uncss'),
+	wiredep = require('wiredep').stream,
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify');
 
-var cssFiles = ['app/css/reset.css', 'app/css/main.css'];
+//build
+gulp.task('build', function() {
+	var assets = useref.assets();
 
-gulp.task('default', function() {
-	gulp.src(cssFiles)
-		.pipe(concatCss("main.css"))
-		.pipe(minifyCss())
-		.pipe(rename('main.min.css'))
-		.pipe(gulp.dest('dist/css/'))
-		.pipe(notify('Done!'));
+	return gulp.src('app/*.html')
+		.pipe(assets)
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', minifyCss))
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest('dist'));
+});
+//bower 
+gulp.task('bower', function() {
+	gulp.src('app/index.html')
+		.pipe(wiredep({
+			directory : "app/bower"
+		}))
+		.pipe(gulp.dest('app'));
 });
 
+// connect
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app',
+		livereload: true
+	});
+});
+
+// css
+gulp.task('css', function() {
+	gulp.src('./app/scss/*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer('last 15 versions'))
+		.pipe(rename('main.css'))
+		.pipe(gulp.dest('./app/css'))
+		.pipe(connect.reload());
+});
+
+//html
+gulp.task('html', function() {
+	gulp.src('./app/index.html')
+	.pipe(connect.reload());
+});
+
+// watch
 gulp.task('watch', function() {
-	gulp.watch(cssFiles, ['default'])
+	gulp.watch('app/scss/*.scss', ['css']);
+	gulp.watch('app/index.html', ['html']);
+	gulp.watch('bower.json', ['bower']);
 });
+
+// default
+gulp.task('default', ['connect', 'html', 'css', 'watch']);
